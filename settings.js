05d26ba7fa054c6bb281095fcb8d53ce4e474fb8
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-03-12 12:35:25 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzmY9um6SOgT1PRU1J4gr3HbJtMivWFWBvAPZqiP9pjSVobOdqvsxE0esBYE6TSy9I8DTHjN3gGdt5/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [36.9129725892497, -121.754540388535];
+const mapCenter = [36.9110240628667, -121.821200746205];
 const mapZoom = 12;
 
 // Marker icon height and width
