a8186364dcd611861b56d760d97c544cbb58401e
gwoolston <gwoolston@me.com>
2024-03-12 14:04:27 -0400

    Update settings.js
    update center

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzmY9um6SOgT1PRU1J4gr3HbJtMivWFWBvAPZqiP9pjSVobOdqvsxE0esBYE6TSy9I8DTHjN3gGdt5/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [36.910246860080946, -121.75636746649849];
+const mapCenter = [36.90995651744821, -121.89880727464121];
 const mapZoom = 12;
 
 // Marker icon height and width
