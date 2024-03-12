694f8fbf57a2c141c7b1aab793b489661d514146
gwoolston <gwoolston@me.com>
2024-03-12 14:03:04 -0400

    Update settings.js
    update initial view

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzmY9um6SOgT1PRU1J4gr3HbJtMivWFWBvAPZqiP9pjSVobOdqvsxE0esBYE6TSy9I8DTHjN3gGdt5/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [36.9091343147002, -121.755301151745];
-const mapZoom = 11;
+const mapCenter = [36.910246860080946, -121.75636746649849];
+const mapZoom = 12;
 
 // Marker icon height and width
 const iconHeight = 30;
