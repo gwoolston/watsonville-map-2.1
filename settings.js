c8d25051ee2d17410f9ec3c1e844ab2c53afe41a
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-04-29 22:13:40 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,5 +7,5 @@ const mapCenter = [36.9110240628667, -121.821200746205];
 const mapZoom = 12;
 
 // Marker icon height and width
-const iconHeight = 80px;
-const iconWidth = 70px;
+const iconHeight = 85;
+const iconWidth = 70;
