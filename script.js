8a19536dbd7d016b20bb3fcd50b2c3347fad5749
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-05-02 11:24:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ var addMarkers = function(data) {
       {
         icon: L.icon({
           iconUrl: d.Icon,
-          iconSize: [25, 30],
+          iconSize: [35, 35],
           iconAnchor: [12.5, 41],
           className: 'br1',
         }),
